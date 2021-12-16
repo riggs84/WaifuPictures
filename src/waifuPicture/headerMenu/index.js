fca57feb905017c1391f76fef8443a2444656796
viktoriurkov <VVYurkov@alfabank.ru>
2021-12-16 20:13:38 +0300

    feat(deploy): fix navigation

--- src/waifuPicture/headerMenu/index.js
+++ src/waifuPicture/headerMenu/index.js
@@ -3,15 +3,16 @@ import OptionsMenu from 'react-native-option-menu';
 import ManageWallpaper, {TYPE} from 'react-native-manage-wallpaper';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const HeaderMenu = ({router}) => {
+const HeaderMenu = ({uri}) => {
   const setWallpaper = type => {
-    ManageWallpaper.setWallpaper({uri: router.params.uri}, () => {}, type);
+    ManageWallpaper.setWallpaper({uri}, () => {}, type);
   };
 
   return (
     <OptionsMenu
-      button={<Icon name={'ellipsis-v'} />}
-      buttonStyle={{width: 32, height: 20, margin: 7.5, resizeMode: 'contain'}}
+      customButton={
+        <Icon name={'ellipsis-v'} size={30} style={{marginRight: 10}} />
+      }
       destructiveIndex={1}
       options={['Save as Home', 'Save as Lock', 'Save as Home and Lock']}
       actions={[
