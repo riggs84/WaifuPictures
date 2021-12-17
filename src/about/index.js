2c38f5faf7422debbc3de6cf2c4b0e63fc81e2ee
viktoriurkov <VVYurkov@alfabank.ru>
2021-12-17 17:43:54 +0300

    feat(deploy): add about page

--- src/about/index.js
+++ src/about/index.js
@@ -3,28 +3,29 @@ import {StyleSheet, Text, View} from 'react-native';
 
 const About = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        This applications allows you to choose picture and set is as wallpaper
-      </Text>
-      <Text>
-        This application uses open source resource for image loading which you
-        can set as device wallpaper
-      </Text>
-      <Text>Developed by Viktor Iurkov</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Text>Developed by: Viktor Iurkov</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 100,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    borderColor: 'grey',
   },
 });
 
